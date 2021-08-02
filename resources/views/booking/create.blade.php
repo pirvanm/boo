184b9f7deec1a1e3f056f3b9d4dd259c7da4ef0d
Marian P <marian.pirvan@easysales.ro>
2021-08-02 11:50:33 +0300

    extra

--- resources/views/booking/create.blade.php
+++ resources/views/booking/create.blade.php
@@ -1,4 +1,6 @@
 <!-- Define a iteration  -->
 @foreach ($slots as $slot) 
 <!-- For each value of array, each key do this : -->
+{{slot}}
+<!-- use {{}} for display , similar with echo, display function --> 
 @endforeach
