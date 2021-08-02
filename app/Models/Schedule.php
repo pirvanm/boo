a0ba3092bd122857b6dc5e21367b35766731ba3f
Marian P <marian.pirvan@easysales.ro>
2021-08-02 11:00:35 +0300

    manipulate time format for model

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Schedule extends Model
 {
     use HasFactory;
+
+    protected $casts = [
+    	'data' => 'datetime',
+    	'start_time' => 'datetime',
+    	'end_time' => 'datetime'
+    ]
 }
