654ee58bedc4b8bf9e24bbc34da3996ddc3d671f
Marian P <marian.pirvan@easysales.ro>
2021-08-02 11:51:28 +0300

    show on new line values

--- resources/views/booking/create.blade.php
+++ resources/views/booking/create.blade.php
@@ -1,6 +1,6 @@
 <!-- Define a iteration  -->
 @foreach ($slots as $slot) 
 <!-- For each value of array, each key do this : -->
-{{slot}}
+{{slot}} <br>
 <!-- use {{}} for display , similar with echo, display function --> 
 @endforeach
