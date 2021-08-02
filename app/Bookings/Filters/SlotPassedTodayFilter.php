897e54b21047369c5edc741984660a79cac29c5d
Marian P <marian.pirvan@easysales.ro>
2021-08-02 12:27:47 +0300

    show if we use mehods of utlitary class

--- app/Bookings/Filters/SlotPassedTodayFilter.php
+++ app/Bookings/Filters/SlotPassedTodayFilter.php
@@ -2,9 +2,13 @@
 
 
 use App\Bookings\Filter;
+use App\Bookings\TImeSlotGenerator;
+
 
 
 class SlotsPassedTodayFilter implements  Filter 
 {
-	
+	public function apply(TImeSlotGenerator $generator, CarbonPeriod $interval) {
+		dd('filter');
+	}
 }
