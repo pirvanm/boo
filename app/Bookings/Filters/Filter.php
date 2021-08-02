bef941c26fd5147c260530ffa16a94a47591355a
Marian P <marian.pirvan@easysales.ro>
2021-08-02 12:15:02 +0300

    define namespace and use external class in interface

--- app/Bookings/Filters/Filter.php
+++ app/Bookings/Filters/Filter.php
@@ -1,5 +1,14 @@
 <?php 
 
+
+
+namespace App\Bookings;
+
+
+use Carbon\CarbonPeriod;
+use App\Bookings\TimeSlotGenerator;
+
+
 interface Filter {
-	public function apply(TimeSlot Generator $generator, Carbon $interval)
+	public function apply(TimeSlot Generator $generator, CarbonPeriod $interval)
 }
