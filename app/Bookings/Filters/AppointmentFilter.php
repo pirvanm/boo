c033983b9a133d571c4054b71f2bb317b4d83420
Marian P <marian.pirvan@easysales.ro>
2021-08-02 14:54:46 +0300

    show appointment filter by start and end date

--- app/Bookings/Filters/AppointmentFilter.php
+++ app/Bookings/Filters/AppointmentFilter.php
@@ -23,6 +23,23 @@ class AppointmentFilter implements  Filter
 	 {
 	 	// define a clousoure 
 	$interval->addFilter( function ($slot) use ($generator)  {
+		foreach ($this->appointments as $appointments) {
+			if(
+				$slot->between(
+				$appointment->date->setTimeFrom(
+					$appointment->start_time 
+				),
+					$appointment->date->setTimeFrom(
+						$appointment->end_time )
+			)
+		)
+		) {
+
+	}
+
+		}
+
+		return true;
 	
 
 	});	
