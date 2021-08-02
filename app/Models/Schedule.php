8321aa9f341d66efffc71ff0480f88b0c40d85d4
Marian P <marian.pirvan@easysales.ro>
2021-08-02 13:24:43 +0300

    define relationship between  2 table via models and aferent methods

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\ScheduleUnavailability;
+
 class Schedule extends Model
 {
     use HasFactory;
@@ -13,5 +15,10 @@ class Schedule extends Model
     	'data' => 'datetime',
     	'start_time' => 'datetime',
     	'end_time' => 'datetime'
-    ]
+    ];
+
+
+    public function unvailablities() {
+    	return $this->hasMany(ScheduleUnavailability::class);
+    }
 }
