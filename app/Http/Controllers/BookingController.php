28d039b3845191726c7056862ba7424f626974a0
Marian P <marian.pirvan@easysales.ro>
2021-08-02 11:45:54 +0300

    generate interval of time in minute from 15 to 15 minutes

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -8,6 +8,12 @@ use Illuminate\Http\Request;
 class BookingController extends Controller
 {
     public function __invoke () {
+
+
+    	$slots = CarbonInterval::minutes(15)
+    	->toPerios(now(), now()->addDay());
+
+    	
     	return view('bookings.create')
 
     }
