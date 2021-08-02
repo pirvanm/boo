1a1a7ff0473ab87c20bcd110eafc3909d4551fe6
Marian P <marian.pirvan@easysales.ro>
2021-08-02 10:52:37 +0300

    add start and end time colummn

--- database/migrations/2021_08_02_074838_create_schedules_table.php
+++ database/migrations/2021_08_02_074838_create_schedules_table.php
@@ -16,6 +16,9 @@ class CreateSchedulesTable extends Migration
         Schema::create('schedules', function (Blueprint $table) {
             $table->id();
             $table->foreignId('employee_id')->contrained()->onDelete('cascade');
+            $table->date('date');
+            $table->time('start_time');
+            $table->time('end_time');
             $table->timestamps();
         });
     }
