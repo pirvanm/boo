d6d37c2a47f9c90fad394cee6f0c865b4a5f535f
Marian P <marian.pirvan@easysales.ro>
2021-08-02 12:01:02 +0300

    move code out of controller and refactor for a clean code

--- app/Bookings/TimeSlotGenerator.php
+++ app/Bookings/TimeSlotGenerator.php
@@ -6,7 +6,12 @@ class TimeSlotGenerator {
 	protected $interval;
 
 	public function __construct(Schedule $schedule) {
-		
+		//show by start and end date
+    	$this->interval = CarbonInterval::minutes(15)
+    	->toPeriod(
+    		$schedule->date->setTimeFrom($schedule->start_time),
+    		$schedule->date->setTimeFrom($schedule->end_time);
+
 	}
 	// use this to about dry and "spaghety code in controller "
 }
