088552e0970d5c75adbfd7eb37512781285a2bb6
Marian P <marian.pirvan@easysales.ro>
2021-08-02 14:18:15 +0300

    tell what date can be store(fillable) in db & define type of data (cast)'

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Appointment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+    	'date',
+    	'start_time',
+    	'end_time',
+    	'client_name',
+    	'client_email'
+    ];
+
+    protected $casts = [
+    	'data' => 'datetime',
+    	'start_time' => 'datetime',
+    	'end_time' => 'datetime'
+    	]
 }
