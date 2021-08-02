b6525931c46d56f894956aa120626eb7e8b4221b
Marian P <marian.pirvan@easysales.ro>
2021-08-02 14:45:04 +0300

    make extendable collection of appoinment from Filter to Controller

--- app/Bookings/Filters/AppointmentFilter.php
+++ app/Bookings/Filters/AppointmentFilter.php
@@ -11,6 +11,12 @@ use App\Bookings\TImeSlotGenerator;
 
 class AppointmentFilter implements  Filter 
 {
+	public $appointments; 
+
+	public function __construct(Colllection $appointments) {
+		$this->appointments = $appointments;
+	}
+	
 	public function apply(TimeSlotGenerator $generator, CarbonPeriod $interval)
 	 {
 	 	// define a clousoure 
