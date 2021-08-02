72b3c88ecc51a91875141b7fd7e194b011871b94
Marian P <marian.pirvan@easysales.ro>
2021-08-02 11:59:51 +0300

    make extentable class

--- app/Bookings/TimeSlotGenerator.php
+++ app/Bookings/TimeSlotGenerator.php
@@ -2,5 +2,11 @@
 namespace App\Bookings;
 
 class TimeSlotGenerator {
+
+	protected $interval;
+
+	public function __construct(Schedule $schedule) {
+		
+	}
 	// use this to about dry and "spaghety code in controller "
 }
